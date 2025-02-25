99db1be63cc777396cf702dd0ba68ef301a97eeb
tiago moreira <tiagowolf11@gmail.com>
2025-02-25 12:31:16 -0300

    Atualização

--- src/edu/tiago/ContaBanco/ContaTerminal.java
+++ src/edu/tiago/ContaBanco/ContaTerminal.java
@@ -1,8 +1,24 @@
 package edu.tiago.ContaBanco;
 
+import java.util.Scanner;
+
+
 public class ContaTerminal {
 	public static void main (String[]args) {
-		System.out.println("Tiago");
+		int numero = 1021;
+		String agencia;
+		String nomeCliente = "Tiago Moreira";
+		double saldo = 237.48;
+		
+		Scanner ag = new Scanner(System.in);
+		
+		System.out.println("Por favor, digite o número da agêancia!");
+		
+		agencia = ag.nextLine();
+		
+		System.out.println("Olá  " +nomeCliente+ ", obriagado por criar uma conta em nosso banco, sua agencia é " 
+		+ agencia +", conta " +numero+ " e seu saldo "+saldo+" já está disponível para saque"); 
+		
 	}
 
 }
